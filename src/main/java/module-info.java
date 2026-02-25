c51b010b814025c31931172de239597973366ed2
Ian-LS-Cesar <iansantoscesar@gmail.com>
2026-02-24 21:55:44 -0300

    Git init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.grupo8.indicehash {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.grupo8.indicehash to javafx.fxml;
+    exports com.grupo8.indicehash;
+}
